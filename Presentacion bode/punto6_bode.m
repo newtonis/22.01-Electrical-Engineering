5331297e916b24bb20ce52280125cf775faef467
jmestanza <jmestanza@itba.edu.ar>
2018-06-04 21:59:21 -0300

    documentado

--- Presentacion bode/punto6_bode.m
+++ Presentacion bode/punto6_bode.m
@@ -1,8 +1,8 @@
 H1=tf([1],[1/17.857 1]);
 H2=tf([1 0],[1/186666648.8 1]);
 H=-190*H1*H2;
-%[num,den]=tfdata(H);
-%bodenum= cell2mat(num);
-%bodeden= cell2mat(den);
-%bode_as(bodenum,bodeden);
-bode(H,{1e-20,1e30});
+[num,den]=tfdata(H);
+bodenum= cell2mat(num);
+bodeden= cell2mat(den);
+bode_as(bodenum,bodeden);
+%bode(H,{1e-20,1e30});
