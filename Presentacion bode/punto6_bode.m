54986894df6b15d590f36beda241b5cd650b3f4a
jmestanza <jmestanza@itba.edu.ar>
2018-06-20 18:54:14 -0300

    listas las simulaciones

--- Presentacion bode/punto6_bode.m
+++ Presentacion bode/punto6_bode.m
@@ -1,8 +1,12 @@
-H1=tf([1],[1/17.857 1]);
-H2=tf([1 0],[1/186666648.8 1]);
-H=-190*H1*H2;
-[num,den]=tfdata(H);
-bodenum= cell2mat(num);
-bodeden= cell2mat(den);
-bode_as(bodenum,bodeden);
+%H1=tf([1],[1/17.857 1]);
+%H2=tf([1 0],[1/186666648.8 1]);
+%H=-190*H1*H2;
+s=tf('s');
+H= s^2/2-s+1;
+H
+bode(H)
+%[num,den]=tfdata(H);
+%bodenum= cell2mat(num);
+%bodeden= cell2mat(den);
+%bode_as(bodenum,bodeden);
 %bode(H,{1e-20,1e30});
