f8d25c2afd089cee554cc6125d0da508af048525
jmestanza <jmestanza@itba.edu.ar>
2018-06-21 20:20:39 -0300

    creo que esta

--- Presentacion bode/punto1_bode.m
+++ Presentacion bode/punto1_bode.m
@@ -5,14 +5,15 @@ wc=100;
 w={0.1*wc 10*wc};
 %w es la banda de frecuencias en las que queremos que nos muestre el
 %grafico
-x_ =[0.1 , 0.2 ,0.3 ,0.5 , 0.7 ,1.0];
+x_ =[0 , 0.1 ,1.0];
 %la función tf nos arma el polinomio (transfer function)
 % el primer argumento es el numerador y el segundo el denominador
 H=tf(zeros(1,1,length(x_))); 
 asympcurv = false;
 %nota: con la curva asintotica no tiene la restriccion de w
 for i =1:length(x_)
-    H(:,:,i)=tf([1],[(1/wc)^2 x_(i)/wc 1]);
+%    H(:,:,i)=tf([1],[(1/wc)^2 x_(i)/wc 1]);
+    H(:,:,i)=tf([1/(wc)^2 0 0],[(1/wc)^2 x_(i)/wc 1]);
     if ~asympcurv
         bode(H(:,:,i),w);
         hold on;
