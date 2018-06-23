4eb6cc772cd161657647e7534b96ec0cb757cce0
Marcelo Regueira <mregueira@itba.edu.ar>
2018-06-22 21:45:46 -0300

    corregido gustavo

--- TP6/codigoDiagramas.m
+++ TP6/codigoDiagramas.m
@@ -29,26 +29,26 @@ title('Diagrama tensiones de fase');
 % compass(IF_MAT);
 % title('Caso L-L-L - Diagrama fasorial U-V');
 
-%Caso R/L-R/L-R/L%
-IRS = 1.48*exp(i*0);
-IST = 1.48*exp(i*(-120)*pi/180);
-ITR = 1.38*exp(i*(-240)*pi/180);
+% %Caso R/L-R/L-R/L%
+% IRS = 1.48*exp(i*(-90)*pi/180);
+% IST = 1.48*exp(i*(-210)*pi/180);
+% ITR = 1.38*exp(i*(-330)*pi/180);
+% IR = IRS-ITR;
+% IS = IST-IRS;
+% IT = ITR-IST;
+% IF_MAT = [IRS IST ITR V_MAT];
+% IL_MAT = [IR IS IT];
+% compass(IF_MAT);
+% title('Caso R/L-R/L-R/L - Diagrama fasorial U-V');
+
+%Caso R-R/L-L%
+IRS = 0.36*exp(i*0);
+IST = 1.47*exp(i*(-210)*pi/180);
+ITR = 1.24*exp(i*(-330)*pi/180);
 IR = IRS-ITR;
 IS = IST-IRS;
 IT = ITR-IST;
 IF_MAT = [IRS IST ITR V_MAT];
 IL_MAT = [IR IS IT];
 compass(IF_MAT);
-title('Caso R/L-R/L-R/L - Diagrama fasorial U-V');
-
-% %Caso R-R/L-L%
-% IRS = 0.36*exp(i*0);
-% IST = 1.47*exp(i*(-210)*pi/180);
-% ITR = 1.24*exp(i*(-330)*pi/180);
-% IR = IRS-ITR;
-% IS = IST-IRS;
-% IT = ITR-IST;
-% IF_MAT = [IRS IST ITR];
-% IL_MAT = [IR IS IT];
-% compass(IF_MAT);
-% title('Caso R-R/L-L - Diagrama fasorial U-V');
+title('Caso R-R/L-L - Diagrama fasorial U-V');
